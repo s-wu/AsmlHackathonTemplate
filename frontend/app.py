509b082866970213786503fefa1d06c46578ddd6
J.Jin <J.Jin@spilgames.com>
2018-10-12 19:27:47 +0200

    CHANGE add the table.

--- frontend/app.py
+++ frontend/app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
+import requests
+
 
 @app.route('/')
 def index():
@@ -9,7 +11,19 @@ def index():
 @app.route('/save', methods=['GET', 'POST'])
 def save():
     if request.method == 'POST':
-        print(request.get_json())
+        res = request.get_json()
+        output = ""
+        if 'res1' in res:
+            for x in res.get("res1", []):
+                for i in range(len(x)):
+                    for j in range(len(x[0])):
+                        output += str(x[i][j])
+                    output += "\n"
+            output += ";"
+        else:
+            output = res.get("res2", "")
+        print(output)
+        # requests.post()
         resp = jsonify(success=True)
         return resp
 
